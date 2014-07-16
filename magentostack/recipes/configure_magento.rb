afb6425c0466a4e4fe2ee99db402e5e4721559c0
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-07-16 04:13:55 +0000

    Writing configuration

--- magentostack/recipes/configure_magento.rb
+++ magentostack/recipes/configure_magento.rb
@@ -3,38 +3,43 @@ node[:deploy].each do |application, deploy|
 
   ruby_block "Create dynamic Magento configuration file for #{application}" do
     block do
-      File.open("/tmp/settings_#{application}.csv", 'w') { |file|
-        file.write("Handler,Param2,Param2,Param3,#{deploy[:environment]}\n")
+      if deploy.key?(:environment) then
+        File.open("/tmp/settings_#{application}.csv", 'w') { |file|
+          file.write("Handler,Param2,Param2,Param3,#{deploy[:environment]}\n")
 
-        file.write("# Database,,,\n")
-        file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/dbname,,#{deploy[:database][:database]}\n")
-        file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/host,,#{deploy[:database][:host]}\n")
-        file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/password,,#{deploy[:database][:password]}\n")
-        file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/username,,#{deploy[:database][:username]}\n")
+          if deploy[:database]
+            file.write("# Database,,,\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/dbname,,#{deploy[:database][:database]}\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/host,,#{deploy[:database][:host]}\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/password,,#{deploy[:database][:password]}\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/username,,#{deploy[:database][:username]}\n")
+          end
 
-        # Assuming we have a single Redis server
-
-        if deploy[:settings][:redis_cache_port]
-          file.write("# Cache Backend,,,\n")
-          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/server,,#{node[:opsworks][:layers][:redis][:instances][0][:private_ip]}\n")
-          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/port,,#{deploy[:settings][:redis_cache_port]}\n")
-        end
-
-        if deploy[:settings][:redis_session_port]
-          file.write("# Sessions Storage,,,\n")
-          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/host,,#{node[:opsworks][:layers][:redis][:instances][0][:private_ip]}\n")
-          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/port,,#{deploy[:settings][:redis_session_port]}\n")
-        end
-      }
+          # Assuming we have a single Redis server
+          if node[:opsworks][:layers][:redis][:instances].first
+            redis_ip = node[:opsworks][:layers][:redis][:instances].first[1][:private_ip].to_s
+            if deploy[:settings][:redis_cache_port]
+              file.write("# Cache Backend,,,\n")
+              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/server,,#{redis_ip}\n")
+              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/port,,#{deploy[:settings][:redis_cache_port].to_s}\n")
+            end
+            if deploy[:settings][:redis_session_port]
+              file.write("# Sessions Storage,,,\n")
+              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/host,,#{redis_ip}\n")
+              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/port,,#{deploy[:settings][:redis_session_port]}\n")
+            end
+          end
+        }
+      end
     end
   end
 
   execute "Apply dynamic Magento environment settings to #{application}" do
     user "deploy"
-    cwd "#{deploy[:deploy_to]}/current/#{deploy[:document_root]"
+    cwd "#{deploy[:deploy_to]}/current/#{deploy[:document_root]}"
     command "../tools/apply.php '#{deploy[:environment]}' '/tmp/settings_#{application}.csv'"
     action :run
-    only_if do File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:document_root]/index.php") end
+    only_if do File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:document_root]}/index.php") end
   end
 
 end
