74cbcbf0817a201a8f160935a75dcb2d017adce2
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-08-04 10:41:31 -0700

    Added dependency to cron cookbook

--- magentostack/metadata.rb
+++ magentostack/metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures magentostack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "cron"
+
+supports "ubuntu"
