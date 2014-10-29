57415011ebc5ea46bf27e9563d57c8d51d059981
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-10-29 21:42:43 +0000

    Using a different key to skip creating settings.csv

--- magentostack/recipes/configure_magento.rb
+++ magentostack/recipes/configure_magento.rb
@@ -1,65 +1,66 @@
 
 node[:deploy].each do |application, deploy|
 
-  Chef::Log.info("Variable 'deploy': #{deploy.inspect}")
-  Chef::Log.info("Variable 'application': #{application.inspect}")
+  # Chef::Log.info("Variable 'deploy': #{deploy.inspect}")
+  # Chef::Log.info("Variable 'application': #{application.inspect}")
   
   ruby_block "Create dynamic Magento configuration file for #{application}" do
+    only_if do deploy.key?(:application)
     block do
-      if deploy.key?(:environment_name) then
-        File.open("/tmp/settings_#{application}.csv", 'w') { |file|
-          file.write("Handler,Param2,Param2,Param3,#{deploy[:environment_name]}\n")
+      File.open("/tmp/settings_#{application}.csv", 'w') { |file|
+        file.write("Handler,Param2,Param2,Param3,#{deploy[:environment_name]}\n")
 
-          if deploy[:database]
-            file.write("# Database,,,\n")
-            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/dbname,,#{deploy[:database][:database]}\n")
-            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/host,,#{deploy[:database][:host]}\n")
-            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/password,,#{deploy[:database][:password]}\n")
-            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/username,,#{deploy[:database][:username]}\n")
+        if deploy[:database]
+          file.write("# Database,,,\n")
+          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/dbname,,#{deploy[:database][:database]}\n")
+          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/host,,#{deploy[:database][:host]}\n")
+          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/password,,#{deploy[:database][:password]}\n")
+          file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/resources/default_setup/connection/username,,#{deploy[:database][:username]}\n")
+        end
+
+        print "\n\n" + deploy.inspect + "\n\n"
+
+        # Assuming we have a single Redis server
+        if node[:opsworks][:layers][:redis][:instances].first
+          redis_ip = node[:opsworks][:layers][:redis][:instances].first[1][:private_ip].to_s
+          if deploy.key?(:settings) && deploy[:settings].key?(:redis_cache_port)
+            file.write("# Cache Backend,,,\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend,,Cm_Cache_Backend_Redis\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/server,,#{redis_ip}\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/port,,#{deploy[:settings][:redis_cache_port].to_s}\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/database,,0\n")
           end
-
-          print "\n\n" + deploy.inspect + "\n\n"
-
-          # Assuming we have a single Redis server
-          if node[:opsworks][:layers][:redis][:instances].first
-            redis_ip = node[:opsworks][:layers][:redis][:instances].first[1][:private_ip].to_s
-            if deploy.key?(:settings) && deploy[:settings].key?(:redis_cache_port)
-              file.write("# Cache Backend,,,\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend,,Cm_Cache_Backend_Redis\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/server,,#{redis_ip}\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/port,,#{deploy[:settings][:redis_cache_port].to_s}\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/cache/backend_options/database,,0\n")
-            end
-            if deploy.key?(:settings) && deploy[:settings].key?(:redis_session_port)
-              file.write("# Sessions Storage,,,\n")
-              file.write("Est_Handler_XmlFile,app/etc/modules/Cm_RedisSession.xml,/config/modules/Cm_RedisSession/active,,true\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/session_save,,db\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/host,,#{redis_ip}\n")
-              file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/port,,#{deploy[:settings][:redis_session_port]}\n")
-            end
+          if deploy.key?(:settings) && deploy[:settings].key?(:redis_session_port)
+            file.write("# Sessions Storage,,,\n")
+            file.write("Est_Handler_XmlFile,app/etc/modules/Cm_RedisSession.xml,/config/modules/Cm_RedisSession/active,,true\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/session_save,,db\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/host,,#{redis_ip}\n")
+            file.write("Est_Handler_XmlFile,app/etc/local.xml,/config/global/redis_session/port,,#{deploy[:settings][:redis_session_port]}\n")
           end
+        end
 
-          # dynamically add more settings
-          if deploy[:env_settings]
-            file.write("# Settings added via chef json,,,\n")
-            deploy[:env_settings].each do |env_setting|
-              file.write("#{env_setting[:handler]},#{env_setting[:param1]},#{env_setting[:param2]},#{env_setting[:param3]},#{env_setting[:value]}\n")
-            end
+        # dynamically add more settings
+        if deploy[:env_settings]
+          file.write("# Settings added via chef json,,,\n")
+          deploy[:env_settings].each do |env_setting|
+            file.write("#{env_setting[:handler]},#{env_setting[:param1]},#{env_setting[:param2]},#{env_setting[:param3]},#{env_setting[:value]}\n")
           end
+        end
 
-        }
-      end
+      }
     end
   end
 
-  magento_basepath="#{deploy[:deploy_to]}/current/#{deploy[:document_root]}/"
+  # magento_basepath="#{deploy[:deploy_to]}/current/#{deploy[:document_root]}/"
+  # 'document_root' is not always populated (e.g. when another app is deployed)
+  magento_basepath="#{deploy[:deploy_to]}/current/htdocs/"
 
   execute "Apply dynamic Magento environment settings to #{application}" do
     user "deploy"
     cwd magento_basepath
     command "../tools/apply.php '#{deploy[:environment_name]}' '/tmp/settings_#{application}.csv'"
     action :run
-    only_if do deploy.key?(:environment_name) && File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:document_root]}/index.php") end
+    only_if do deploy.key?(:application) && File.exists?("#{magento_basepath}index.php") end
   end
 
   Chef::Log.info("First server in layer IP: #{node[:opsworks][:layers]['php-app'][:instances].sort.first[1][:private_ip].inspect}")
