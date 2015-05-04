6be6003ad14f3708efe6a9dde048d98af0dfe858
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-05-04 12:34:47 +0000

    Fix quoting

--- magentostack/recipes/elb_register.rb
+++ magentostack/recipes/elb_register.rb
@@ -8,7 +8,7 @@ node['additional-elbs']['elbs'].each do |elb|
   Chef::Log.info("Elb: #{elb}")
 
   execute "Register instance #{currentInstanceId} with ELB #{elb}" do
-    command "AWS_ACCESS_KEY_ID=#{node['additional-elbs']['aws_access_key_id']}; AWS_SECRET_ACCESS_KEY=#{node['additional-elbs']['aws_secret_access_key']}; aws --region #{node[:opsworks][:instance][:region]} elb register-instances-with-load-balancer --load-balancer-name #{elb} --instances '{\"instance_id\":\"#{node[:opsworks][:instance][:aws_instance_id]}\"}'"
+    command "AWS_ACCESS_KEY_ID=\"#{node['additional-elbs']['aws_access_key_id']}\"; AWS_SECRET_ACCESS_KEY=\"#{node['additional-elbs']['aws_secret_access_key']}\"; aws --region #{node[:opsworks][:instance][:region]} elb register-instances-with-load-balancer --load-balancer-name #{elb} --instances '{\"instance_id\":\"#{node[:opsworks][:instance][:aws_instance_id]}\"}'"
     user "deploy"
   end
 
