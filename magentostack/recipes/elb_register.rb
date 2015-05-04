8c1434d42f8bc34c3deee155acf96536ad95f2d3
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-05-04 11:22:40 +0000

    Removed loop

--- magentostack/recipes/elb_register.rb
+++ magentostack/recipes/elb_register.rb
@@ -1,8 +1,12 @@
 Chef::Log.info("Recipe magentostack::elb_register")
 
+currentInstanceId = node[:opsworks][:instance][:aws_instance_id]
+
+Chef::Log.info("Current instance Id: #{currentInstanceId}")
+
 # current instances
-currentIds = node[:opsworks][:layers]['php-app'][:instances].sort.collect{|i| i['aws_instance_id'] }
-Chef::Log.info("Current instance Ids: #{currentIds}")
+#currentIds = node[:opsworks][:layers]['php-app'][:instances].sort.collect{|i| i['aws_instance_id'] }
+#Chef::Log.info("Current instance Ids: #{currentIds}")
 
 node['additional-elbs']['elbs'].each do |elb|
   Chef::Log.info("Elb: #{elb}")
