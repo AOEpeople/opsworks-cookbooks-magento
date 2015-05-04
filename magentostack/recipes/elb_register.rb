19612367159702d003433c8e5a04f510e7b1df82
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-05-04 12:51:32 +0000

    Moved env vars

--- magentostack/recipes/elb_register.rb
+++ magentostack/recipes/elb_register.rb
@@ -8,7 +8,11 @@ node['additional-elbs']['elbs'].each do |elb|
   Chef::Log.info("Elb: #{elb}")
 
   execute "Register instance #{currentInstanceId} with ELB #{elb}" do
-    command "AWS_ACCESS_KEY_ID=\"#{node['additional-elbs']['aws_access_key_id']}\"; AWS_SECRET_ACCESS_KEY=\"#{node['additional-elbs']['aws_secret_access_key']}\"; aws --region #{node[:opsworks][:instance][:region]} elb register-instances-with-load-balancer --load-balancer-name #{elb} --instances \"#{node[:opsworks][:instance][:aws_instance_id]}\""
+    command "aws --region #{node[:opsworks][:instance][:region]} elb register-instances-with-load-balancer --load-balancer-name #{elb} --instances \"#{node[:opsworks][:instance][:aws_instance_id]}\""
+    environment(
+      'AWS_ACCESS_KEY_ID' => node['additional-elbs']['aws_access_key_id'],
+      'AWS_SECRET_ACCESS_KEY' => node['additional-elbs']['aws_secret_access_key']
+    )
     user "deploy"
   end
 
