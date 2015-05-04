ad16746162068e7cb8489024b4ad22ead0a1a64b
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-05-04 11:10:53 +0000

    Added ELB register (WIP)

--- magentostack/recipes/elb.rb
+++
@@ -1,5 +0,0 @@
-Chef::Log.info("Recipe magentostack::elb")
-
-# current instances
-currentIds = node[:opsworks][:layers]['php-app'][:instances].sort.collect{|i| i['aws_instance_id'] }
-Chef::Log.info("Current instance Ids: #{currentIds}")
---
+++ magentostack/recipes/elb_register.rb
@@ -0,0 +1,14 @@
+Chef::Log.info("Recipe magentostack::elb")
+
+# current instances
+currentIds = node[:opsworks][:layers]['php-app'][:instances].sort.collect{|i| i['aws_instance_id'] }
+Chef::Log.info("Current instance Ids: #{currentIds}")
+
+node['additional-elbs']['elbs'].each do |elb|
+  Chef::Log.info("Elb: #{elb}")
+end
+
+
+
+#"aws_access_key_id"
+#"aws_secret_access_key"
