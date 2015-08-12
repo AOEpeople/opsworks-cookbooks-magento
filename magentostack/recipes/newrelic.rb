434c07bc747b57a5129c488a17343e1b98481482
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-08-12 03:30:24 +0000

    Added newrelic appname for CLI

--- magentostack/recipes/newrelic.rb
+++ magentostack/recipes/newrelic.rb
@@ -7,7 +7,15 @@ include_recipe 'newrelic::php-agent'
 file "/etc/php5/cli/conf.d/newrelic.ini" do
   action :delete
 end
+
 file "/etc/php5/apache2/conf.d/newrelic.ini" do
   action :delete
   notifies :restart, "service[#{node['newrelic']['php-agent']['web_server']['service_name']}]", :delayed
+end
+
+bash 'configure newrelic appname for cli' do
+  code <<-EOH
+    sed -i '/newrelic/d' /etc/php5/cli/php.ini
+    echo "newrelic.appname = \"#{node['newrelic']['application_monitoring']['appname']} CLI\"" >> /etc/php5/cli/php.ini
+    EOH
 end
