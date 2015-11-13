1c1064e561c2c54a4dc7100683e4e6b3628fa5e7
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-11-13 21:24:41 +0000

    Added log files to cloudwatch

--- magentostack/recipes/setup.rb
+++ magentostack/recipes/setup.rb
@@ -24,13 +24,39 @@ execute "install mcrypt" do
 end
 
 node[:deploy].each do |application, deploy|
-  template "/etc/logrotate.d/magentologs_#{application}" do
-    backup false
-    source "logrotate.erb"
-    cookbook 'magentostack'
-    owner "root"
-    group "root"
-    mode 0644
-    variables( :log_dirs => ["#{deploy[:deploy_to]}/shared/var/log" ] )
-  end
+
+    template "/etc/logrotate.d/magentologs_#{application}" do
+        backup false
+        source "logrotate.erb"
+        cookbook 'magentostack'
+        owner "root"
+        group "root"
+        mode 0644
+        variables( :log_dirs => ["#{deploy[:deploy_to]}/shared/var/log" ] )
+    end
+
+    node['cwlogs']['logfiles']["magento_system_log_#{application}"] = {
+        :log_stream_name => "magento_system_log_#{application}",
+        :log_group_name => "#{application}",
+        :file => "#{deploy[:deploy_to]}/shared/var/log/system.log",
+        :datetime_format => '%Y-%m-%dT%H:%M:%S%z',
+        :initial_position => 'end_of_file'
+    }
+
+    node['cwlogs']['logfiles']["apache_access_log_#{application}"] = {
+        :log_stream_name => "apache_access_log_#{application}",
+        :log_group_name => "#{application}",
+        :file => "/var/log/apache2/#{application}-access.log",
+        :datetime_format => '%d/%b/%Y:%H:%M:%S %z',
+        :initial_position => 'end_of_file'
+    }
+
+    node['cwlogs']['logfiles']["apache_error_log_#{application}"] = {
+        :log_stream_name => "apache_error_log_#{application}",
+        :log_group_name => "#{application}",
+        :file => "/var/log/apache2/#{application}-error.log",
+        :datetime_format => '%d/%b/%Y:%H:%M:%S %z',
+        :initial_position => 'end_of_file'
+    }
+
 end
